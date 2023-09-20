4a3b96d21c3385577680153faf1f5ab8eebeeb67
dvera17 <dsvera174@gmail.com>
2023-09-20 17:29:11 -0500

    feat: :sparkles: Implementacion de los endpoints de autenticacion
    Implementacion de los endpoints de autenticacion

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { config } from "dotenv";
+import routerAuth from './routes/auth.routes.js';
 config();
 const app = express();
 app.set('port', process.env.PORT_SERVER);
 app.use(express.json());
+
+app.use('/auth', routerAuth)
 
 export default app;
