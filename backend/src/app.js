f4f2fa3a0b1f66895b3aa887e494eff0d57cf58b
dvera17 <dsvera174@gmail.com>
2023-09-21 17:01:36 -0500

    feat: :sparkles: Configuracion de cors
    Configuracion de cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import { config } from "dotenv";
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import routerAuth from './routes/auth.routes.js';
 import { tokenVerification } from './helpers/jwt.js';
 config();
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+}
+
 const app = express();
 app.set('port', process.env.PORT_SERVER);
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 app.use('/auth', routerAuth)
 
