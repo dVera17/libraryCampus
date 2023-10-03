16a605485c56372b6c5933a24462be3e4aa36684
Nirclaw <caicedonicolas@gmail.com>
2023-10-03 22:20:11 +0000

    feat: :lipstick: Se mejora la HomePage
    Se mejora la HomePage

--- frontend/src/pagesUser/HomeUser.jsx
+++ frontend/src/pagesUser/HomeUser.jsx
@@ -39,6 +39,81 @@ export default function HomeUser() {
                 <>
                     <NavbarPages />
                     <TemporaryDrawerUser />
+                    <div className="body-home-page">
+                        <div className="main-content">
+                            <header className="main-header">
+                                <h1>Library</h1>
+                                <p className="eslogan">Tu fuente de inspiración</p>
+                            </header>
+                            <main>
+                                <section className="intro">
+                                    <p>
+                                        La librería Campus es una librería independiente que ofrece una
+                                        amplia selección de libros para todos los gustos. Nos comprometemos a
+                                        ofrecer un servicio al cliente de primera y a proporcionar a nuestros
+                                        clientes una experiencia de compra agradable.
+                                    </p>
+                                </section>
+                            </main>
+                        </div>
+                        <footer className="footer-section">
+                            <div className="container">
+                                <div className="footer-cta pt-5 pb-5">
+                                    <div className="row">
+                                        <div className="col-xl-4 col-md-4 mb-30">
+                                            <div className="single-cta">
+                                                <i className="fas fa-map-marker-alt"></i>
+                                                <div className="cta-text">
+                                                    <h4>Find us</h4>
+                                                    <span>1010 Avenue, sw 54321, chandigarh</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-xl-4 col-md-4 mb-30">
+                                            <div className="single-cta">
+                                                <i className="fas fa-phone"></i>
+                                                <div className="cta-text">
+                                                    <h4>Call us</h4>
+                                                    <span>9876543210 0</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-xl-4 col-md-4 mb-30">
+                                            <div className="single-cta">
+                                                <i className="far fa-envelope-open"></i>
+                                                <div className="cta-text">
+                                                    <h4>Mail us</h4>
+                                                    <span>mail@info.com</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="copyright-area">
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+                                            <div className="copyright-text">
+                                                <p>Copyright &copy; 2018, All Right Reserved</p>
+                                            </div>
+                                        </div>
+                                        <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                                            <div className="footer-menu">
+                                                <ul>
+                                                    <li><a href="#">Home</a></li>
+                                                    <li><a href="#">Terms</a></li>
+                                                    <li><a href="#">Privacy</a></li>
+                                                    <li><a href="#">Policy</a></li>
+                                                    <li><a href="#">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </footer>
+                    </div>
                 </>
             )}
         </>
